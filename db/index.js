8bf42c19f5d279cd64ff583cc9112a8e2a56af4e
unknown <dbupworks@gmail.com>
2021-03-09 02:56:18 +0000

    updated

--- db/index.js
+++ db/index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
 // Connection URI
-const uri = "mongodb+srv://wang:lasQ7q350LVsRQWm@cluster0.asfo1.mongodb.net/peachly_twitter_bots?authSource=admin&replicaSet=atlas-4jftde-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+//const uri = "mongodb+srv://wang:lasQ7q350LVsRQWm@cluster0.asfo1.mongodb.net/peachly_twitter_bots?authSource=admin&replicaSet=atlas-4jftde-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+const uri = "mongodb+srv://wang:lasQ7q350LVsRQWm@cluster0.asfo1.mongodb.net/peachly_fans_twitter?authSource=admin&replicaSet=atlas-4jftde-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
 const database_name = "peachly_twitter_bots"
+
 
 mongoose
     .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
